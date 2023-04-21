6c580019070b0dc2fa935a491afd88c1b537f2db
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-21 15:36:54 -0400

    added delte function, not finished

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
-import { addNewItem, getAllitems } from './src/items.js'
+import { addNewItem, getAllitems, deleteItem } from './src/items.js'
 
 const app = express()
 app.use(cors())
@@ -9,5 +9,6 @@ app.use(express.json())
 
 app.post("/items", addNewItem)
 app.get("/items", getAllitems)
+app.delete("/items/:id", deleteItem)
 
 export const api = functions.https.onRequest(app)
--- functions/src/items.js
+++ functions/src/items.js
@@ -13,3 +13,16 @@ export async function getAllitems(req, res) {
     const itemsClean = itemsMessy.docs.map(doc => ({...doc.data(), id: doc.id}))
     res.send(itemsClean)
 }
+
+
+export async function deleteItem(req, res) {
+
+}
+
+// export async function deleteEmployee(req, res) {
+//     const { id } = req.params;
+//     // const id = req.params.id
+//     await coll.doc(id).delete();
+//     res.status(202).send("Employee has been deleted")
+//   }
+  
